a5a6f3b50e343decb3160e2c8e5fb891bc29d7ae
苏兰 <sulanlalala@163.com>
2018-03-29 14:53:28 +0800

    下备注测试类的提交

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
+		//测试的单个类提交
 		SpringApplication.run(Demo1Application.class, args);
 	}
 }
